889acf5db56cce3b97e9e5517a62b60c174c7a55
vikasthakurnitpatna <vikasthakur17121999@gmail.com>
2022-02-01 19:08:24 +0530

    file modified

--- pr4.js
+++ pr4.js
@@ -4,7 +4,7 @@ const btns=document.querySelectorAll(".btn");
 btns.forEach(function(btn){
     btn.addEventListener("click",function(e){
         let classlists = e.currentTarget.classList
-        if(classlists.contains("decrease")){
+       {if(classlists.contains("decrease")){
               count--;
             }
         else if(classlists.contains("increase")){
@@ -13,7 +13,17 @@ btns.forEach(function(btn){
         else{
             count=0;
         }
-            mn.textContent=count;
-    
+    }
+       { if(count>0){
+            mn.style.color="blue"
+        }
+        else if(count<0){
+            mn.style.color="red"
+        }
+        else{
+            mn.style.color="green"
+        }
+    }
+    mn.textContent=count;
     })
 })
